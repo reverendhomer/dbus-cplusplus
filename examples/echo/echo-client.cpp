f83f758634b7b78bd2ec90af9af07b2a48152a38
Reverend Homer <mk.43.ecko@gmail.com>
2019-01-15 20:57:19 +0300

    echo-client: remove using namespace std

--- examples/echo/echo-client.cpp
+++ examples/echo/echo-client.cpp
@@ -9,8 +9,6 @@
 #include <stdio.h>
 #include <cstring>
 
-using namespace std;
-
 static const char *ECHO_SERVER_NAME = "org.freedesktop.DBus.Examples.Echo";
 static const char *ECHO_SERVER_PATH = "/org/freedesktop/DBus/Examples/Echo";
 
@@ -21,69 +19,53 @@ EchoClient::EchoClient(DBus::Connection &connection, const char *path, const cha
 
 void EchoClient::Echoed(const DBus::Variant &value)
 {
-  cout << "!";
+  std::cout << "!";
 }
 
 static const size_t THREADS = 3;
-
 static bool spin = true;
-
 EchoClient *g_client = NULL;
-
 DBus::Pipe *thread_pipe_list[THREADS];
-
 DBus::BusDispatcher dispatcher;
-DBus::DefaultTimeout *timeout;
 
 void *greeter_thread(void *arg)
 {
   char idstr[16];
-  size_t i = (size_t) arg;
-
+  auto i = reinterpret_cast<size_t>(arg);
   snprintf(idstr, sizeof(idstr), "%lu", pthread_self());
-
   thread_pipe_list[i]->write(idstr, strlen(idstr) + 1);
-
-  cout << idstr << " done (" << i << ")" << endl;
-
+  std::cout << idstr << " done (" << i << ")" << std::endl;
   return NULL;
 }
 
 void niam(int sig)
 {
   spin = false;
-
   dispatcher.leave();
 }
 
 void handler1(const void *data, void *buffer, unsigned int nbyte)
 {
-  char *str = (char *) buffer;
-  cout << "buffer1: " << str << ", size: " << nbyte << endl;
+  auto str = static_cast<char *>(buffer);
+  std::cout << "buffer1: " << str << ", size: " << nbyte << std::endl;
   for (int i = 0; i < 30 && spin; ++i)
-  {
-    cout << "call1: " << g_client->Hello(str) << endl;
-  }
+    std::cout << "call1: " << g_client->Hello(str) << std::endl;
 }
 
 void handler2(const void *data, void *buffer, unsigned int nbyte)
 {
-  char *str = (char *) buffer;
-  cout << "buffer2: " << str << ", size: " << nbyte << endl;
+  auto str = static_cast<char *>(buffer);
+  std::cout << "buffer2: " << str << ", size: " << nbyte << std::endl;
   for (int i = 0; i < 30 && spin; ++i)
-  {
-    cout << "call2: " << g_client->Hello(str) << endl;
-  }
+    std::cout << "call2: " << g_client->Hello(str) << std::endl;
 }
 
 void handler3(const void *data, void *buffer, unsigned int nbyte)
 {
-  char *str = (char *) buffer;
-  cout << "buffer3: " << str << ", size: " << nbyte << endl;
+  auto str = static_cast<char *>(buffer);
+  std::cout << "buffer3: " << str << ", size: " << nbyte << std::endl;
   for (int i = 0; i < 30 && spin; ++i)
-  {
-    cout << "call3: " << g_client->Hello(str) << endl;
-  }
+    std::cout << "call3: " << g_client->Hello(str) << std::endl;
 }
 
 int main()
@@ -98,7 +80,7 @@ int main()
   // increase DBus-C++ frequency
   new DBus::DefaultTimeout(100, false, &dispatcher);
 
-  DBus::Connection conn = DBus::Connection::SessionBus();
+  auto conn = DBus::Connection::SessionBus();
 
   EchoClient client(conn, ECHO_SERVER_PATH, ECHO_SERVER_NAME);
   g_client = &client;
@@ -109,11 +91,11 @@ int main()
   thread_pipe_list[1] = dispatcher.add_pipe(handler2, NULL);
   thread_pipe_list[2] = dispatcher.add_pipe(handler3, NULL);
   for (size_t i = 0; i < THREADS; ++i)
-    pthread_create(threads + i, NULL, greeter_thread, (void *) i);
+    pthread_create(&threads[i], NULL, greeter_thread, (void *) i);
 
   dispatcher.enter();
 
-  cout << "terminating" << endl;
+  std::cout << "terminating\n";
 
   for (size_t i = 0; i < THREADS; ++i)
     pthread_join(threads[i], NULL);
